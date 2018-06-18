42161a3342c9d748b420d71d74fc3d8fa0b89992
Bruno Borges <bruno.borges@gmail.com>
2018-06-18 10:13:27 -0700

    Add HttpMethod enum and return HttpResponseMessage for fluent API (#12)
    * Add HttpMethod enum
    * Add HttpStatus enum
    * Define a HttpWebHookType enum
    * Remove unecessary generic type on HttpResponseMessage and define HttpResponseMessage.Builder
    * fix javadoc

--- src/main/java/com/microsoft/azure/functions/annotation/HttpTrigger.java
+++ src/main/java/com/microsoft/azure/functions/annotation/HttpTrigger.java
@@ -11,6 +11,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpWebHookType;
+
 /**
  * <p>The HttpTrigger annotation is applied to Azure functions that will be triggered by a call to the HTTP endpoint that
  * the function is located at. The HttpTrigger annotation should be applied to a method parameter of one of the following
@@ -110,7 +113,7 @@ public @interface HttpTrigger {
      *
      * @return An array containing all valid HTTP methods.
      */
-    String[] methods() default {};
+    HttpMethod[] methods() default {};
 
     /**
      * <p>Determines what keys, if any, need to be present on the request in order to invoke the function. The authorization
@@ -130,22 +133,9 @@ public @interface HttpTrigger {
     AuthorizationLevel authLevel() default AuthorizationLevel.FUNCTION;
 
     /**
-     * <p>Configures the HTTP trigger to act as a webhook receiver for the specified provider. Don't set the methods
-     * property if you set this property. The webhook type can be one of the following values:</p>
+     * Defines the WebHook type for this HttpTrigger
      *
-     * <ul>
-     *     <li><strong>genericJson</strong>: A general-purpose webhook endpoint without logic for a specific provider.
-     *     This setting restricts requests to only those using HTTP POST and with the {@code application/json} content
-     *     type.</li>
-     *     <li><strong>github</strong>: The function responds to
-     *     <a href="https://developer.github.com/webhooks/">GitHub webhooks</a>. Do not use the {@link #authLevel()}
-     *     property with GitHub webhooks.</li>
-     *     <li><strong>slack</strong>: The function responds to
-     *     <a href="https://api.slack.com/outgoing-webhooks">Slack webhooks</a>. Do not use the {@link #authLevel()}
-     *     property with Slack webhooks.</li>
-     * </ul>
-     *
-     * @return A string representing the desired webhook type.
+     * @return A webhook type.
      */
-    String webHookType() default "";
+    HttpWebHookType webHookType() default HttpWebHookType.NONE;
 }
