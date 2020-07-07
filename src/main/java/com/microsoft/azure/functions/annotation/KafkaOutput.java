59fcd2100d421e6428ba04e8d191009e74d578f1
Tsuyoshi Ushio <ushio@simplearchitect.com>
2020-07-06 20:09:29 -0700

    adding datatype and name for the rigger and output for passing test

--- src/main/java/com/microsoft/azure/functions/annotation/KafkaOutput.java
+++ src/main/java/com/microsoft/azure/functions/annotation/KafkaOutput.java
@@ -20,6 +20,21 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface KafkaOutput {
     /**
+     * The variable name used in function.json.
+     * @return The variable name used in function.json.
+     */
+    String name();
+
+    /**
+     * <p>Defines how Functions runtime should treat the parameter value. Possible values are:</p>
+     * <ul>
+     *     <li>"" or string: treat it as a string whose value is serialized from the parameter</li>
+     *     <li>binary: treat it as a binary data whose value comes from for example OutputBinding&lt;byte[]&gt;</li>
+     * </ul>
+     * @return The dataType which will be used by the Functions runtime.
+     */
+    String dataType() default "";    
+    /**
      * Gets the Topic.
      * @return
      */
