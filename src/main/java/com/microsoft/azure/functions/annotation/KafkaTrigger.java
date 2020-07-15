362c0343ff844dac90ed9a9b9aa9c33896050aca
Tsuyoshi Ushio <ushio@simplearchitect.com>
2020-07-14 17:36:57 -0700

    Add comment for the release (#121)
    * Add comment for the release
    
    * Fit the context to the Kfaka
    
    * fix ci fails
    
    * update version and fix the grammar
    
    * apply the same change to the KafkaTrigger
    
    * add new line for commenting
    
    * nit
    
    * update comment for POJO. Pojo is available only for cardinality.one not for cardinality.many
    
    * update the sentense to understand more

--- src/main/java/com/microsoft/azure/functions/annotation/KafkaTrigger.java
+++ src/main/java/com/microsoft/azure/functions/annotation/KafkaTrigger.java
@@ -16,11 +16,42 @@ import java.lang.annotation.ElementType;
 
 
 /**
- * <p>Annotation for KafkaTrigger bindings</p>
+ * <p>
+ * Place this on a parameter whose value would come from Kafka, and causing the method to run
+ * when Kafka event is consumed. The parameter type can be one of the following:
+ * </p>
+ *
+ * <ul>
+ *     <li>Any native Java types such as int, String, byte[]</li>
+ *     <li>Nullable values using Optional&lt;T&gt;</li>
+ *     <li>Any POJO type, currently supported only for Cardinality.One</li>
+ * </ul>
+ *
+ * <p>
+ * The following example shows a Java function that is invoked when messages are consumed with
+ * the specified topic, brokerList, and consumerGroup on a Kafka cluster.
+ * </p>
+ *
+ * <pre>
+ * {@literal @}FunctionName("KafkaTrigger-Java")
+ * public void run(
+ *    {@literal @}KafkaTrigger(name = "kafkaTrigger",
+ *                      topic = "users", 
+ *                      brokerList="broker:29092",
+ *                      consumerGroup="functions")
+ *                      List&lt;Map&lt;String, String&gt;&gt; kafkaEventData,
+ *     final ExecutionContext context
+ * ) {
+ *     context.getLogger().info(kafkaEventData);
+ * }
+ * </pre>
+ *
+ * @since 1.4.0
  */
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface KafkaTrigger {
+
     /**
      * The variable name used in function code for the request or request body.
      * 
@@ -29,17 +60,23 @@ public @interface KafkaTrigger {
     String name();
 
     /**
-     * Gets the Topic.
+     * Defines the Topic.
+     * 
+     * @return The topic.
      */
     String topic();
 
     /**
-     * Gets or sets the BrokerList.
+     * Defines the BrokerList.
+     * 
+     * @return The brokerList.
      */
     String brokerList();
 
     /**
-     * Gets or sets the EventHub connection string when using KafkaOutput protocol header feature of Azure EventHubs.
+     * Defines the EventHub connection string when using KafkaOutput protocol header feature of Azure EventHubs.
+     * 
+     * @return The EventHub connection string.
      */
     String eventHubConnectionString() default "";
     /**
@@ -47,18 +84,24 @@ public @interface KafkaTrigger {
      * Choose 'One' if the input is a single message or 'Many' if the input is an array of messages.
      * If you choose 'Many', please set a dataType. 
      * Default: 'One'
+     * 
+     * @return The cardinality.
      */
     Cardinality cardinality() default Cardinality.ONE;
+
     /**
-     * DataType for the Cardinality settings. If you set the cardinality as Cardinality.MANY, Azure Functions Host will deserialize
-     * the kafka events as an array of this type.
-     * Allowed values: string, binary, stream
-     * Default: ""
+     * <p>Defines how Functions runtime should treat the parameter value. Possible values are:</p>
+     * <ul>
+     *     <li>"": get the value as a string, and try to deserialize to actual parameter type like POJO</li>
+     *     <li>string: always get the value as a string</li>
+     *     <li>binary: get the value as a binary data, and try to deserialize to actual parameter type byte[]</li>
+     * </ul>
+     * @return The dataType which will be used by the Functions runtime.
      */
     String dataType() default "";
 
     /**
-     * Gets or sets the consumer group.
+     * Defines the consumer group.
      */
     String consumerGroup();
 
@@ -66,12 +109,16 @@ public @interface KafkaTrigger {
      * SASL mechanism to use for authentication.
      * Allowed values: Gssapi, Plain, ScramSha256, ScramSha512
      * Default: PLAIN
+     * 
+     * @return The broker authentication mode.
      */
     BrokerAuthenticationMode authenticationMode() default BrokerAuthenticationMode.NOTSET;
 
     /**
      * SASL username with the PLAIN and SASL-SCRAM-.. mechanisms
      * Default: ""
+     * 
+     * @return The SASL username.
      */
     String username() default "";
 
@@ -80,12 +127,16 @@ public @interface KafkaTrigger {
      * Default: ""
      *
      * security.protocol in librdkafka
+     * 
+     * @return The SASL password.
      */
     String password() default "";
 
     /**
-     * Gets or sets the security protocol used to communicate with brokers
+     * Defines the security protocol used to communicate with brokers
      * default is PLAINTEXT
+     * 
+     * @return The security protocol.
      */
     BrokerProtocol protocol() default BrokerProtocol.NOTSET;
 
@@ -93,24 +144,32 @@ public @interface KafkaTrigger {
      * Path to client's private key (PEM) used for authentication.
      * Default ""
      * ssl.key.location in librdkafka
+     * 
+     * @return The ssl key location.
      */
     String sslKeyLocation() default "";
 
-        /**
+    /**
      * Path to CA certificate file for verifying the broker's certificate.
      * ssl.ca.location in librdkafka
+     * 
+     * @return The path to CA certificate file. 
      */
     String sslCaLocation() default "";
 
     /**
      * Path to client's certificate.
      * ssl.certificate.location in librdkafka
+     * 
+     * @return The ssl certificate location.
      */
     String sslCertificateLocation() default "";
 
     /**
      * Password for client's certificate.
      * ssl.key.password in librdkafka
+     * 
+     * @return The ssl key password.
      */
     String sslKeyPassword() default "";   
 }
