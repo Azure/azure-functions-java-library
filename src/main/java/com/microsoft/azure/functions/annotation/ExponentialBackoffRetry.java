a388c98152f74d2faea10f3b7269e0fa3bd296d0
Tsuyoshi Ushio <ushio@simplearchitect.com>
2020-11-11 09:31:08 -0800

    Add Retry annotation

---
+++ src/main/java/com/microsoft/azure/functions/annotation/ExponentialBackoffRetry.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
+package com.microsoft.azure.functions.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * <p>
+ * Defines an exponential backoff retry strategy, where the delay between retries
+ * will get progressively larger, limited by the max/min specified.</p>
+ *
+ * @since 1.0.0
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+public @interface ExponentialBackoffRetry {
+    /**
+     * The maximum number of retries that will be attempted.
+     * @return The maximum retry count.
+     */
+    int maxRetryCount();
+    /**
+     * The minimum delay interval.
+     * @return The minimum retry delay.
+     */
+    String minimumInterval();
+    /**
+     * The maximum delay interval.
+     * @return The maximum retry delay.
+     */
+    String maximumInterval();
+}
---
+++ src/main/java/com/microsoft/azure/functions/annotation/FixedDelayRetry.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
+package com.microsoft.azure.functions.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * <p>
+ * Defines a retry strategy where a fixed delay is used between retries.</p>
+ *
+ * @since 1.0.0
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+public @interface FixedDelayRetry {
+    /**
+     * The maximum number of retries that will be attempted.
+     * @return The delay between retries.
+     */
+    int maxRetryCount();
+    /**
+     * The delay between retries.
+     * @return The delay interval.
+     */
+    String delayInterval();
+}
