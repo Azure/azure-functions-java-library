6d4d5d9c80b78c7afe1e3ca6e7a5ad628a736041
Shreyas Gopalakrishna <shreyasg@microsoft.com>
2022-02-01 09:34:51 -0600

    Updating java library pipeline with the new library architecture and end to end tests

--- build.ps1
+++ build.ps1
@@ -50,6 +50,10 @@ $libraryVersion = $matches[1]
 Write-Host "libraryVersion: " $libraryVersion
 
 # Download azure-functions-core-tools
+$FUNC_RUNTIME_VERSION = '4'
+$arch = [System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture.ToString().ToLowerInvariant()
+$os = if ($IsWindows) { "win" } else { if ($IsMacOS) { "osx" } else { "linux" } }
+
 $currDir =  Get-Location
 $skipCliDownload = $false
 if($args[0])
@@ -64,11 +68,11 @@ Remove-Item -Force ./Azure.Functions.Cli.zip -ErrorAction Ignore
 Remove-Item -Recurse -Force ./Azure.Functions.Cli -ErrorAction Ignore
 
 Write-Host "Downloading Functions Core Tools...."
-Invoke-RestMethod -Uri 'https://functionsclibuilds.blob.core.windows.net/builds/2/latest/version.txt' -OutFile version.txt
+Invoke-RestMethod -Uri "https://functionsclibuilds.blob.core.windows.net/builds/$FUNC_RUNTIME_VERSION/latest/version.txt" -OutFile version.txt
 Write-Host "Using Functions Core Tools version: $(Get-Content -Raw version.txt)"
 Remove-Item version.txt
 
-$url = "https://functionsclibuilds.blob.core.windows.net/builds/2/latest/Azure.Functions.Cli.win-x86.zip"
+$url = "https://functionsclibuilds.blob.core.windows.net/builds/$FUNC_RUNTIME_VERSION/latest/Azure.Functions.Cli.$os-$arch.zip"
 $output = "$currDir\Azure.Functions.Cli.zip"
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($url, $output)
@@ -78,32 +82,31 @@ Expand-Archive ".\Azure.Functions.Cli.zip" -DestinationPath ".\Azure.Functions.C
 }
 $Env:Path = $Env:Path+";$currDir\Azure.Functions.Cli"
 
-# Generate HttpTrigger Function via archetype version built above
-md -Name ciTestDir
-Push-Location -Path "./ciTestDir" -StackName libraryDir
-Write-Host "Generating project with archetype" 
-cmd.exe /c '.\..\mvnGenerateArchetype.bat' $atchetypeVersion
-Pop-Location -StackName "libraryDir"
-
-#Build HttpTrigger Function
-
-Push-Location -Path "./ciTestDir/e2etestproject" -StackName libraryDir
-Remove-Item -Recurse -Force "src/test" -ErrorAction Ignore
-cmd.exe /c .\..\..\updateVersions.bat $libraryVersion $pluginVersion
-StopOnFailedExecution
-#Update versions in the HttpTrigger pom.xml
-cmd.exe /c '.\..\..\mvnBuild.bat'
-StopOnFailedExecution
-Pop-Location -StackName "libraryDir"
-
 # Clone and build azure-functions-java-worker
 git clone https://github.com/azure/azure-functions-java-worker -b dev
 Push-Location -Path "./azure-functions-java-worker" -StackName libraryDir
-Write-Host "Updating azure-functions-java-worker to use current version of library" 
+Write-Host "Updating azure-functions-java-worker to use current version of the java library"
 
 cmd.exe /c .\..\updateVersions.bat $libraryVersion
-Write-Host "Building azure-functions-java-worker" 
+Write-Host "Building azure-functions-java-worker"
 cmd.exe /c '.\mvnBuild.bat'
-StopOnFailedExecution    
+StopOnFailedExecution
+Pop-Location -StackName "libraryDir"
+
+# Update core tools with the new Java worker
+Write-Host "Replacing Java worker binaries in the Core Tools..."
+Get-ChildItem -Path "./azure-functions-java-worker/target/*" -Include 'azure*' -Exclude '*shaded.jar','*tests.jar' | ForEach-Object {
+  Copy-Item $_.FullName "./Azure.Functions.Cli/workers/java/azure-functions-java-worker.jar" -Force -Verbose
+}
+Copy-Item -Path ".\Azure.Functions.Cli" -Destination ".\azure-functions-java-worker\Azure.Functions.Cli" -Recurse
+
+# Updating end to end tests with the new library
+Push-Location -Path "./azure-functions-java-worker/endtoendtests" -StackName libraryDir
+Write-Host "Updating azure-functions-java-worker endtoendtests to use current version of the java library"
+
+cmd.exe /c .\..\..\updateVersions.bat $libraryVersion $pluginVersion
+Write-Host "Building azure-functions-java-worker end to end tests"
+cmd.exe /c '.\..\..\mvnBuild.bat'
+StopOnFailedExecution
 Pop-Location -StackName "libraryDir"
 
