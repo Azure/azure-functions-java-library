bdcef31e628bff298508c024288aad2030f35e42
Ahmed Muhsin <ahmedmuhsin@microsoft.com>
2025-09-03 12:01:16 -0500

    fix some styling bugs

--- src/main/java/com/microsoft/azure/functions/annotation/McpToolProperty.java
+++ src/main/java/com/microsoft/azure/functions/annotation/McpToolProperty.java
@@ -1,6 +1,15 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
 package com.microsoft.azure.functions.annotation;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Defines a strongly-typed input property for an MCP tool function parameter.
